9b16968c3f04d38025165b64305ddba5743c8c6f
jamo254 <70902444+jamo254@users.noreply.github.com>
2021-09-08 02:12:06 +0300

    Created Models, testimonial pages and migrations

--- admin.py
+++ admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+
+class MyMovie(admin.ModelAdmin):
+    list_display = ("name", "description", "year", "star", "show")
+    list_filter = ('star', 'show')
+
+
+admin.site.register(Movie, MyMovie)
--- views.py
+++ views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
+from .models import Movie
 
 # Create your views here.
 
+
 def home(request):
-    return render(request, 'home.html', {})
+
+    movie_data = Movie.objects.all()
+    return render(request, 'home.html', {"movie": movie_data})
+
 
 def testimonials(request):
     return render(request, 'testimonials.html')
+
+
+def movie(request, movie_id):
+    movie_data = Movie.objects.get(id=movie_id)
+    return render(request, 'movie.html', {"movie": movie_data})
